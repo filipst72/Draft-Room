50c5193d58b88c6f3dbcd536c2fa6a26a100082d
filipst72 <fstojanovic7223rn@raf.rs>
2025-11-21 21:20:10 +0100

    Add files via upload

---
+++ AppCore.java
@@ -0,0 +1,10 @@
+package raf.draft.dsw;
+
+import raf.draft.dsw.core.ApplicationFramework;
+
+public class AppCore {
+    public static void main(String[] args) {
+        ApplicationFramework appCore = ApplicationFramework.getInstance();
+        appCore.initialize();
+    }
+}
